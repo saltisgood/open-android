ada10804c3e9320e315c4d83117e7f289af51a86
Nicholas Stephen <git@nickstephen.com>
2014-02-11 17:23:09 +1100

    Finished updating project (will now build)

--- OpenAndroid/src/main/java/com/nickstephen/openandroid/reflection/InstantiatorFrag.java
+++ OpenAndroid/src/main/java/com/nickstephen/openandroid/reflection/InstantiatorFrag.java
@@ -1,13 +1,14 @@
 package com.nickstephen.openandroid.reflection;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nickstephen.lib.gui.Fragment;
 import com.nickstephen.lib.gui.FragmentUtil;
 import com.nickstephen.openandroid.R;
+
+import org.holoeverywhere.LayoutInflater;
 
 /**
  * Created by Nick on 14/01/14.
